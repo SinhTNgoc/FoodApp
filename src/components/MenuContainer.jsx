c7d182e69f7994fe6cb93aea43554cfcb2c46063
SinhTNgoc <tranngocsinh2211@gmail.com>
2022-07-05 16:55:43 +0700

    Filter-render items

---
+++ src/components/MenuContainer.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { IoFastFood } from "react-icons/io5";
+import { categoriesData } from "../utils/data";
+import { motion } from "framer-motion";
+import RowContainer from "./RowContainer";
+import { useStateValue } from "../context/StateProvider";
+
+const MenuContainer = () => {
+  const [filter, setFilter] = useState("chicken");
+  const [{ foodItems }, dispatch] = useStateValue();
+  return (
+    <section className="w-full" id="menu">
+      <div className="w-full flex flex-col items-center justify-center">
+        <p className="text-2xl font-semibold text-headingColor capitalize mr-auto relative before:absolute before:rounded-lg before:content-[''] before:w-16 before:h-1 before:bottom-0 before:left-0 before:bg-gradient-to-tr from-orange-200 to-orange-600 transition-all duration-100 ease-in-out">
+          Our host dishes
+        </p>
+        <div className="w-full flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
+          {categoriesData &&
+            categoriesData.map((item) => (
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                key={item.id}
+                className={`group ${
+                  filter === item.urlParamName ? "bg-red-500" : "bg-cardOverlay"
+                } w-24 min-w-[94px] h-28 cursor-pointer rounded-md drop-shadow-xl flex flex-col gap-3 items-center justify-center transition-all duration-100 ease-in-out hover:bg-red-500`}
+                onClick={(e) => setFilter(item.urlParamName)}
+              >
+                <div
+                  className={`w-10 h-10 rounded-full flex items-center justify-center ${
+                    filter === item.urlParamName
+                      ? "bg-cardOverlay"
+                      : "bg-red-500"
+                  }  group-hover:bg-cardOverlay`}
+                >
+                  <IoFastFood className="w-5 h-5  text-white  group-hover:text-textColor" />
+                </div>
+                <p
+                  className={`text-base ${
+                    filter === item.urlParamName
+                      ? "text-cardOverlay"
+                      : "text-textColor"
+                  }  group-hover:text-cardOverlay`}
+                >
+                  {item.name}
+                </p>
+              </motion.div>
+            ))}
+        </div>
+        <div className="w-full">
+          <RowContainer
+            flag={false}
+            data={foodItems?.filter((item) => item.category === filter)}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MenuContainer;
---
+++ src/components/RowContainer.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from "react";
+import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import NotFound from "../img/NotFound.svg";
+
+const RowContainer = ({ flag, data, scrollValue }) => {
+  const rowContainerRef = useRef();
+
+  useEffect(() => {
+    rowContainerRef.current.scrollLeft += scrollValue;
+  }, [scrollValue]);
+
+  return (
+    <div
+      className={`w-full my-12 bg-rowBg flex items-center gap-3 scroll-smooth ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden justify-center flex-wrap"
+      }`}
+      ref={rowContainerRef}
+    >
+      {data && data.length > 0 ? (
+        data.map((item) => (
+          <div
+            key={item.id}
+            className="w-full min-w-full md:w-340 md:min-w-[340px] h-[225px] my-12 bg-gray-200 rounded-lg p-2 backdrop-blur-lg hover:drop-shadow-md flex flex-col justify-between"
+          >
+            <div className="w-full flex items-center justify-between">
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                className="w-40 h-40  -mt-8 drop-shadow-2xl"
+              >
+                <img
+                  src={item?.imageURL}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </motion.div>
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                className="w-6 h-6 rounded-full bg-red-500 flex items-center justify-center cursor-pointer hover:shadow-md "
+              >
+                <MdShoppingBasket className="w-4 h-4 text-white" />
+              </motion.div>
+            </div>
+            <div className="w-full flex flex-col gap-1 items-end justify-end">
+              <p className="text-base text-textColor ">{item?.title}</p>
+              <p className="text-sm text-gray-500 ">{item?.calory} calories</p>
+              <div className="flex items-center gap-8">
+                <p className="text-lg text-gray-500 font-semibold">
+                  <span className="text-sm text-red-500">$</span> {item?.price}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="w-fullflex flex-col items-center justify-center">
+          <img
+            src={NotFound}
+            alt="notfound"
+            className="w-full h-[320px] object-contain"
+          />
+          <p className="text-2xl text-textColor capitalize">Item is not available</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RowContainer;
